97326e174a8247fddbf96decdcd155ae94c92fc9
amakadiya98 <amakadiya98@gmail.com>
2026-02-06 12:38:32 +0530

    add footer and newsletter responsive deisgn with sldier implementations

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,28 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scrolled");
     }
 });
+
+// testimonial-slider
+
+$('.testimonial-slider').owlCarousel({
+    loop:true,
+    margin: 30,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2.2
+        },
+        1100:{
+            items:3
+        }
+    }
+})
